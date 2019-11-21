82cae4fe7396056ac8dc783159cae385daeb6bec
Zman <zes888@gmail.com>
2019-11-21 16:51:05 -0500

    RoomChangingstuff
    getting stuff ready to test room changing

--- PossiblyInMichiGame/Assets/Z's Scripts/Arrow.cs
+++ PossiblyInMichiGame/Assets/Z's Scripts/Arrow.cs
@@ -5,4 +5,22 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     public string roomName; //stores the name of the room the arrow will take you to when clicked
+
+    public SpriteRenderer sr;
+
+    public void Start() //starts invisible
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.color = Color.white;
+    }
+
+    public void Show() //Sets the sprite to be shaded
+    {
+        sr.color = Color.white;
+    }
+
+    public void Hide() //Turns the alpha to 0 making the arow invisible
+    {
+        sr.color = Color.clear;
+    }
 }
