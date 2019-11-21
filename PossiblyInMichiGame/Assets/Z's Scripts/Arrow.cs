c1efaf8323cdfaa114a57f2750fd2d273e3d7311
Zman <zes888@gmail.com>
2019-11-21 16:03:42 -0500

    Added Arrows
    arrows

---
+++ PossiblyInMichiGame/Assets/Z's Scripts/Arrow.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public string roomName; //stores the name of the room the arrow will take you to when clicked
+}
