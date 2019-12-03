451aa36286abf595111fe0bc36e7bcc947532c68
Alex Theologides Rodriguez <atheologidesrodriguez@gmail.com>
2019-12-03 15:26:21 -0500

    AddItem and Inventory Interactions

--- PossiblyInMichiGame/Assets/TR's Scripts/Inventory.cs
+++ PossiblyInMichiGame/Assets/TR's Scripts/Inventory.cs
@@ -7,6 +7,9 @@ public class Inventory : MonoBehaviour
 {
     public bool[] collected; //probably unnecessary in the long run
     public Button[] invButtons;
+    public Item[] myItems;
+    public Sprite[] itemSprites;
+    public Item testBaby;
 
 
     void Start()
@@ -24,48 +27,72 @@ public class Inventory : MonoBehaviour
             }
         }
     }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))//Test to go through inventory and give information about each item
+        {
+            for (int i = 0; i < myItems.Length; i++)
+            {
+                Debug.Log(myItems[i].gameObject.tag + ": " + myItems[i].isCollected);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))//AddItem Test
+        {
+            AddItem(testBaby);
+        }
+    }
 
     public void AddItem(Item thisItem)
     {
         switch (thisItem.gameObject.tag)
         {
-            case "ItemQuarter":
+            case "ItemQuarter": //Not really ever used but just made for completion
                 invButtons[0].interactable = true;
+                invButtons[0].image.sprite = itemSprites[1];
                 collected[0] = true;
                 Destroy(thisItem.gameObject);
                 break;
             case "ItemKey":
                 invButtons[1].interactable = true;
+                invButtons[1].image.sprite = itemSprites[1];
                 collected[1] = true;
                 Destroy(thisItem.gameObject);
                 break;
             case "ItemGum":
                 invButtons[2].interactable = true;
+                invButtons[2].image.sprite = itemSprites[2];
                 collected[2] = true;
                 Destroy(thisItem.gameObject);
                 break;
             case "ItemBaby":
                 invButtons[3].interactable = true;
+                invButtons[3].image.sprite = itemSprites[3];
                 collected[3] = true;
                 Destroy(thisItem.gameObject);
                 break;
-            case "ItemShoe":
+            case "ItemEar":
                 invButtons[4].interactable = true;
+                invButtons[4].image.sprite = itemSprites[4];
                 collected[4] = true;
                 Destroy(thisItem.gameObject);
                 break;
-            case "ItemEar":
+            case "ItemShoe":
                 invButtons[5].interactable = true;
+                invButtons[5].image.sprite = itemSprites[5];
                 collected[5] = true;
                 Destroy(thisItem.gameObject);
                 break;
             case "ItemSnail":
                 invButtons[6].interactable = true;
+                invButtons[6].image.sprite = itemSprites[6];
                 collected[6] = true;
                 Destroy(thisItem.gameObject);
                 break;
             case "ItemPerfume":
                 invButtons[7].interactable = true;
+                invButtons[7].image.sprite = itemSprites[7];
                 collected[7] = true;
                 Destroy(thisItem.gameObject);
                 break;
--- PossiblyInMichiGame/Assets/TR's Scripts/Item.cs
+++ PossiblyInMichiGame/Assets/TR's Scripts/Item.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+
+    public bool isCollected, isKnown;
     // Start is called before the first frame update
     void Start()
     {
