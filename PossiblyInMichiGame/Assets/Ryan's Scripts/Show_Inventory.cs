c321fc97cdd96326d76754e29700130b9188450a
Ryan Ray <rr3149@nyu.edu>
2019-12-13 20:29:26 -0500

    Lot's of Scene Changes: - Repositioned characters - Repositioned backgrounds and room names - Repositioned items - Other characters - 1 character Object Recieved - Inventory UI - Inventory button

---
+++ PossiblyInMichiGame/Assets/Ryan's Scripts/Show_Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//INENT: Make the inventory less obnoxious in the top of the screen
+public class Show_Inventory : MonoBehaviour
+{
+
+    public Button inventoryButton;
+
+    public GameObject inventoryUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+               
+        Button btn = inventoryButton.GetComponent<Button>(); //Getting the button
+        btn.onClick.AddListener(TaskOnClick); //Button calls TaskOnClick
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick()
+    {
+        inventoryUI.SetActive(!inventoryUI.activeSelf);
+    }
+}
